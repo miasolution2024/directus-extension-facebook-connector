838faac5ba7e1c169d6c9bfb0d88cb1ff6770fa5
Hieu Vo Trung <hieu.vo@idealogic.com.vn>
2025-06-24 23:25:15 +0700

    Init project

---
+++ src/APIs/callback.ts
@@ -0,0 +1,104 @@
+import { redirectToErrorPage, redirectToFrontend } from "../helper";
+import {
+  GetintegrationSettingsData,
+  LogInformationEvent,
+  LogIntegrationEvent,
+} from "../services/directus.service";
+import {
+  GetGetLongLiveToken,
+  GetPagesAndSubscribeWebhooks,
+  GetShortLiveToken,
+} from "../services/facebook-graph.service";
+
+export async function handleFacebookCallback(
+  req: any,
+  res: any,
+  services: any,
+  getSchema: any
+) {
+  const code = req.query.code;
+  const integrationSettingsData = await GetintegrationSettingsData(services, req, getSchema);
+
+  if (!code) {
+    const logId = await LogIntegrationEvent(req, services, getSchema, {
+      level: "error",
+      message: `No authorization code received from Facebook.`,
+      context: "handleFacebookCallback",
+      stack_trace: "No authorization code received from Facebook.",
+      user_id: req.accountability ? req.accountability.user : null,
+      request_string: "",
+      response_string: "",
+      timestamp: new Date(),
+    });
+
+    redirectToErrorPage(res, integrationSettingsData.public_directus_url, logId);
+  }
+
+  const { ItemsService } = services;
+  const schema = await getSchema();
+
+  const ominiChannelsService = new ItemsService("omini_channels", {
+    schema,
+    accountability: req.accountability,
+  });
+
+  const redirectUri = `${integrationSettingsData.public_directus_url}/directus-extension-facebook-connector/api/facebook/auth/callback`;
+
+  try {
+    const shortLivedUserAccessToken = await GetShortLiveToken(
+      integrationSettingsData,
+      redirectUri,
+      code
+    );
+
+    await LogInformationEvent(
+      req,
+      services,
+      getSchema,
+      `Short-lived User Access Token: ${shortLivedUserAccessToken}`,
+      "handleFacebookCallback"
+    );
+
+    const userAccessToken = await GetGetLongLiveToken(
+      integrationSettingsData,
+      shortLivedUserAccessToken
+    );
+
+    await LogInformationEvent(
+      req,
+      services,
+      getSchema,
+      `Long-lived User Access Token: ${userAccessToken}`,
+      "handleFacebookCallback"
+    );
+
+    const connectedPagesCount = await GetPagesAndSubscribeWebhooks(
+      ominiChannelsService,
+      integrationSettingsData,
+      userAccessToken
+    );
+
+    await LogInformationEvent(
+      req,
+      services,
+      getSchema,
+      `Connected ${connectedPagesCount} Facebook Page(s) and subscribed to webhooks.`,
+      "handleFacebookCallback"
+    );
+    redirectToFrontend(res, integrationSettingsData.public_directus_url);
+  } catch (error: any) {
+    const logId = await LogIntegrationEvent(req, services, getSchema, {
+      level: "error",
+      message: `An unexpected error occurred during Facebook connection:`,
+      context: "handleFacebookCallback",
+      stack_trace: `An unexpected error occurred during Facebook connection: ${JSON.stringify(
+        error
+      )}`,
+      user_id: req.accountability ? req.accountability.user : null,
+      request_string: "",
+      response_string: "",
+      timestamp: new Date(),
+    });
+    redirectToErrorPage(res, integrationSettingsData.public_directus_url, logId);
+  }
+}
---
+++ src/services/directus.service.ts
@@ -0,0 +1,161 @@
+export interface AppSettings {
+  facebook_app_id: string;
+  facebook_app_secret: string;
+  public_directus_url: string;
+  webhook_verify_token: string;
+  n8n_webhook_url: string;
+}
+
+export interface IntegrationLog {
+  timestamp: Date;
+  level: string;
+  request_string: string;
+  message: string;
+  stack_trace: string;
+  response_string: string;
+  user_id: string;
+  context: string;
+}
+
+export interface OminichannelCreateRequest {
+  page_id: string;
+  page_name: string;
+  token: string;
+  is_enabled: boolean;
+  expired_date: Date;
+  source: OminichannelSource;
+}
+
+export interface OminichannelUpdateRequest {
+  page_name: string;
+  token: string;
+}
+
+export enum OminichannelSource {
+  Facebook = "Facebook",
+  Tiktok = "Tiktok",
+  Zalo = "Zalo",
+}
+
+export async function GetintegrationSettingsData(
+  services: any,
+  req: any,
+  getSchema: any
+): Promise<AppSettings> {
+  const { ItemsService } = services;
+  const schema = await getSchema();
+
+  const integrationSettingsService = new ItemsService("integration_settings", {
+    schema,
+    accountability: req.accountability,
+  });
+
+  try {
+    const integrationSettings = await integrationSettingsService.readByQuery({
+      limit: 1,
+    });
+
+    if (integrationSettings.length === 0) {
+      throw new Error("No intergration settings found in Directus.");
+    }
+    const integrationSettingsData = integrationSettings[0] as AppSettings;
+
+    if (
+      !integrationSettingsData.facebook_app_id ||
+      !integrationSettingsData.facebook_app_secret ||
+      !integrationSettingsData.public_directus_url ||
+      !integrationSettingsData.webhook_verify_token
+    ) {
+      throw new Error("App settings data is empty in Directus.");
+    }
+    return integrationSettingsData;
+  } catch (error) {
+    console.error("Error loading app settings:", error);
+    throw error;
+  }
+}
+
+export async function GetfbPagesService(
+  services: any,
+  req: any,
+  getSchema: any
+): Promise<any> {
+  try {
+    const { ItemsService } = services;
+    const schema = await getSchema();
+
+    const fbPagesService = new ItemsService("fb_pages", {
+      schema,
+      accountability: req.accountability,
+    });
+    return fbPagesService;
+  } catch (error) {
+    console.error("Error loading facebook pages:", error);
+    throw error;
+  }
+}
+
+export async function GetIntegrationLogsService(
+  services: any,
+  req: any,
+  getSchema: any
+): Promise<any> {
+  try {
+    const { ItemsService } = services;
+    const schema = await getSchema();
+
+    const fbPagesService = new ItemsService("integration_logs", {
+      schema,
+      accountability: req.accountability,
+    });
+    return fbPagesService;
+  } catch (error) {
+    console.error("Error loading integration logs:", error);
+    throw error;
+  }
+}
+
+export async function LogIntegrationEvent(
+  services: any,
+  req: any,
+  getSchema: any,
+  logEntry: IntegrationLog
+): Promise<string> {
+  try {
+    const integrationLogsService = await GetIntegrationLogsService(
+      services,
+      req,
+      getSchema
+    );
+
+    const data = await integrationLogsService.createOne(logEntry);
+    return data.id;
+  } catch (error: any) {
+    throw new Error(
+      `Error logging integration event: ${error.message || error}`
+    );
+  }
+}
+
+export async function LogInformationEvent(
+  req: any,
+  services: any,
+  getSchema: any,
+  message: string,
+  context: string = ""
+): Promise<string> {
+  try {
+    return await LogIntegrationEvent(services, req, getSchema, {
+      level: "info",
+      message,
+      context: context,
+      stack_trace: "",
+      user_id: req.accountability ? req.accountability.user : null,
+      request_string: "",
+      response_string: "",
+      timestamp: new Date(),
+    });
+  } catch (error: any) {
+    throw new Error(`Error logging information: ${error.message || error}`);
+  }
+}
